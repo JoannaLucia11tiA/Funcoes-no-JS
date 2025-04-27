089c9657e35f7c0fec689a50114ecb4fe5cd70b6
Nickolas-Zilig-Oliveira <joannalucia25@gmail.com>
2025-04-27 17:49:43 -0300

    6 Exemplos de funções, fim. Por JL

---
+++ main.js
@@ -0,0 +1,58 @@
+/*
+Exemplo 1
+
+function mensagem(){
+    console.log("Seja bem-vindo(a)!")
+    console.log("É um prazer tê-lo aqui.")
+    console.log("Aproveite")
+}
+mensagem()
+*/
+/*
+Exemplo 2 - Função com parâmetros
+
+function somarNumeros(valor1, valor2){
+    let soma = valor1 + valor2
+    console.log(soma)
+}
+somarNumeros(21, 09)
+*/
+/*
+Exemplo 3 - Função com parâmetros
+
+function boasVindas(nome, cargo){
+    console.log(`Seja bem-vindo(a) ${cargo} ${nome}!`)
+}
+boasVindas("Joanna", "Aluna")
+*/
+/*
+Exemplo 4 - Função com return
+
+function somar(valor1, valor2){
+    return valor1 + valor2
+}
+let resultado = somar (21, 09)
+console.log(somar(09, 2008))
+console.log(resultado)
+ */
+/*
+Exemplo 5 - Função com return
+
+function boasVindas(nome, cargo){
+    return `Seja bem-vindo(a) ${cargo} ${nome}!`
+}
+let mensagem = boasVindas("Joanna", "Aluna")
+console.log(mensagem)
+*/
+/*
+Exemplo 6 - Função condicional sem else
+
+function parImpar(valor){
+    if(valor%2===0){
+        return "Par"
+    }
+    return "Ìmpar"
+}
+let verificacao = parImpar(21)
+console.log(verificacao)
+*/
